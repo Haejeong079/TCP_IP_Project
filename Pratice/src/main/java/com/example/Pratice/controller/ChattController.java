bf31284ec67ce377cfb2ad040ea6f1c841328113
양해정 <127839004+Haejeong079@users.noreply.github.com>
2023-12-03 22:34:28 +0900

    채팅 세션, 필요없는부분 주석 or 삭제,signup2.css 추가
    Release notes
    
    기존에는 채팅시에 세션을 안 넣었는데 세션을 넣고 컨트롤러에 모델을 추가하였음. 자동으로 닉네임 값이 등록이 됨 (하지만 닉네임 등록 버튼을 눌러야지 통신이 가능함.)
    → 로그인 페이지에서 아이디 및 비밀번호 찾기 없앰
    → 게시글 검색 기능 삭제
    → 오버캔버스 필요없는 부분 삭제
    → 테이블 호버 줬음.

--- Pratice/src/main/java/com/example/Pratice/controller/ChattController.java
+++ Pratice/src/main/java/com/example/Pratice/controller/ChattController.java
@@ -1,5 +1,8 @@
 package com.example.Pratice.controller;
 
+import com.example.Pratice.entity.Member;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class ChattController {
 
     @RequestMapping("/chatt")
-    public ModelAndView chatt(){
+    public ModelAndView chatt(Model model, HttpSession session){
+        Member user = (Member) session.getAttribute("user");
+        model.addAttribute("user", user.getNickname());
+
         ModelAndView mv = new ModelAndView();
         mv.setViewName("chat");
+
+        model.addAttribute("nickname",user.getNickname());
 
         return mv;
     }
